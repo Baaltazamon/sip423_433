020a09aca4963a48ef4a2267f31313a405a4586e
Илья Палатов <3gpabbiu4eji@mail.ru>
2021-01-18 09:45:48 +0300

    Add project files.

---
+++ sip423_433/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sip423_433.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
